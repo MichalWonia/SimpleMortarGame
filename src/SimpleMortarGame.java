411e78a0b0a901430e2883d042e646b3f5d182a7
Michal Wonia <spodek.mw@gmail.com>
2021-11-28 12:39:26 +0100

    Added method to check if the target has been hit.

--- src/SimpleMortarGame.java
+++ src/SimpleMortarGame.java
@@ -1,19 +1,28 @@
 public class SimpleMortarGame {
 
-    public static double mortarRange(double firingAngleInDegrees, double initialVelocity){
+    public static double mortarRange(double firingAngleInDegrees, double initialVelocity) {
         double range;
         double g = 9.81;
         double angleInRadians = Math.toRadians(firingAngleInDegrees);
-        range = (2*Math.pow(initialVelocity, 2)*Math.sin(angleInRadians)*Math.cos(angleInRadians))/g;
+        // calculating range of the diagonal projection
+        range = (2 * Math.pow(initialVelocity, 2) * Math.sin(angleInRadians) * Math.cos(angleInRadians)) / g;
 
-
-
-    return range;
+        return range;
     }
 
-
+    public static void isObstacleHit(double distanceFromObstacle, double mortarRange) {
+        int margin = 10; // hit margin
+        if (mortarRange > distanceFromObstacle + margin) {
+            System.out.println("Oops!!! The missile flew " + (mortarRange - (distanceFromObstacle + margin)) + " metres too far...");
+        } else if (mortarRange < distanceFromObstacle - margin) {
+            System.out.println("Oops!!! The missile flew " + ((distanceFromObstacle - margin) - mortarRange) + " metres too close...");
+        } else {
+            System.out.println("Boooom!!! You have hit the obstacle !!! :)");
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println(mortarRange(45, 100));
+
+
     }
 }
