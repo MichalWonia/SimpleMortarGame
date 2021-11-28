9f036f0318f80090d0cbe451734d2c35710a9378
Michal Wonia <spodek.mw@gmail.com>
2021-11-28 12:25:56 +0100

    Added method to calculate range of missile.

--- src/SimpleMortarGame.java
+++ src/SimpleMortarGame.java
@@ -1,6 +1,19 @@
 public class SimpleMortarGame {
 
+    public static double mortarRange(double firingAngleInDegrees, double initialVelocity){
+        double range;
+        double g = 9.81;
+        double angleInRadians = Math.toRadians(firingAngleInDegrees);
+        range = (2*Math.pow(initialVelocity, 2)*Math.sin(angleInRadians)*Math.cos(angleInRadians))/g;
+
+
+
+    return range;
+    }
+
+
+
     public static void main(String[] args) {
-        
+        System.out.println(mortarRange(45, 100));
     }
 }
