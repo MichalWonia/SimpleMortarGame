123fa8b20201809137136e54decae7d011bbf54a
Michal Wonia <spodek.mw@gmail.com>
2021-11-28 13:01:00 +0100

    Added method startGame(). Project finished.

--- src/SimpleMortarGame.java
+++ src/SimpleMortarGame.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SimpleMortarGame {
 
     public static double mortarRange(double firingAngleInDegrees, double initialVelocity) {
@@ -6,23 +8,53 @@ public class SimpleMortarGame {
         double angleInRadians = Math.toRadians(firingAngleInDegrees);
         // calculating range of the diagonal projection
         range = (2 * Math.pow(initialVelocity, 2) * Math.sin(angleInRadians) * Math.cos(angleInRadians)) / g;
-
         return range;
     }
 
-    public static void isObstacleHit(double distanceFromObstacle, double mortarRange) {
+    public static boolean isObstacleHit(double distanceFromTarget, double mortarRange) {
         int margin = 10; // hit margin
-        if (mortarRange > distanceFromObstacle + margin) {
-            System.out.println("Oops!!! The missile flew " + (mortarRange - (distanceFromObstacle + margin)) + " metres too far...");
-        } else if (mortarRange < distanceFromObstacle - margin) {
-            System.out.println("Oops!!! The missile flew " + ((distanceFromObstacle - margin) - mortarRange) + " metres too close...");
+        if (mortarRange > distanceFromTarget + margin) {
+            System.out.println("Oops!!! The missile flew " + (mortarRange - (distanceFromTarget + margin)) + " metres too far...");
+            return false;
+        } else if (mortarRange < distanceFromTarget - margin) {
+            System.out.println("Oops!!! The missile flew " + ((distanceFromTarget - margin) - mortarRange) + " metres too close...");
+            return false;
         } else {
             System.out.println("Boooom!!! You have hit the obstacle !!! :)");
+            return true;
         }
     }
 
+    public static void startGame(int numberOfMissiles, int distanceFromTarget){
+
+        boolean gameOver;
+        double firingAngle;
+        double initialVelocity;
+        double range;
+        Scanner sc = new Scanner(System.in);
+
+        while (numberOfMissiles != 0){
+            System.out.println("Distance from target: " + distanceFromTarget);
+            System.out.println("You have " + numberOfMissiles + " missiles");
+
+            System.out.println("Enter the initial velocity [m/s]: ");
+            initialVelocity = sc.nextDouble();
+
+            System.out.println("Enter the firing angle [\u00B0]: ");
+            firingAngle = sc.nextDouble();
+
+            range = mortarRange(firingAngle, initialVelocity);
+            gameOver = isObstacleHit(distanceFromTarget, range);
+            System.out.println();
+            if(gameOver){
+                break;
+            }
+            numberOfMissiles--;
+        }
+        System.out.println("Game over");
+    }
+
     public static void main(String[] args) {
-
-
+        startGame(5, 100);
     }
 }
